94b3cb5557d59fa69b426034d80a6fbd77bce128
Kamrul Hasan <kamrul444216@gmail.com>
2023-04-17 06:52:00 +0600

    login signUp form toggle done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,24 +4,6 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    // <div className="center">
-    //   <div className="form-container">
-    //     <h2 className="form-title">Login</h2>
-    //     <form>
-    //       <div className="form-control">
-    //         <label htmlFor="">Email</label>
-    //         <input
-    //           type="email"
-    //           name="email"
-    //           id="email"
-    //           placeholder="Email"
-    //           required
-    //         />
-    //       </div>
-    //     </form>
-    //   </div>
-    // </div>
-
     <div className="form-container">
       <form>
         <h2>Login</h2>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
 import { Link } from 'react-router-dom';
 
 
 const SignUp = () => {
+
+    const [error, setError] = useState('')
+    const handleSignUp = event =>{
+
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value
+        const password = form.password.value
+        const confirmPassword = form.confirmPassword.value
+        console.log(email, password, confirmPassword);
+
+        if(password !== confirmPassword){
+            setError("Password Not Match")
+            return
+        }
+        else if(password.length < 6){
+            setError('Password must be 6 character')
+            return
+        }
+    }
+
   return (
     <div className="signup-container">
-      <form>
+      <form onSubmit={handleSignUp}>
         <h2>Create an account</h2>
 
         <label htmlFor="email">Email:</label>
@@ -24,11 +45,12 @@ const SignUp = () => {
         <label htmlFor="confirm-password">Confirm Password:</label>
         <input
           type="password"
-          id="confirm-password"
-          name="confirm-password"
+          id="confirmPassword"
+          name="confirmPassword"
           minLength="6"
           required
         />
+        <p className="error">{error}</p>
 
         <input className="signUp" type="submit" value="Sign Up" />
 
