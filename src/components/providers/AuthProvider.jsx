7cda856af6800c007f75fec6f2a830014bcde295
Kamrul Hasan <kamrul444216@gmail.com>
2023-04-19 23:49:02 +0600

    addddded

--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -9,11 +9,14 @@ const auth = getAuth(app)
 const AuthProvider = ({children}) => {
 
     const [user,setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const createUser = (email,password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password);
     }
     const signIn = (email,password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth,email,password);
     }
     const logOut = () => {
@@ -24,6 +27,7 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+            setLoading(false)
         })
         return () => {
             return unsubscribe();
@@ -35,6 +39,7 @@ const AuthProvider = ({children}) => {
         createUser,
         signIn,
         logOut,
+        loading,
     }
 
     return (
